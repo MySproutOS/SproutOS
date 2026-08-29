2c9801bb7477bdb0cf68eb6c985360ebff3ecbc2
Andrew Chen Wang <acwangpython@gmail.com>
2026-08-29 16:45:04 -0400

    Update ECS bootstrap render checksum

--- bin/render-ecs-launch-template-user-data.test.sh
+++ bin/render-ecs-launch-template-user-data.test.sh
@@ -7,8 +7,8 @@ cleanup() { unlink "$rendered" 2>/dev/null || true; }
 trap cleanup EXIT
 TOFU_DIR="$ROOT/tofu" "$ROOT/bin/render-ecs-launch-template-user-data.sh" sproutos >"$rendered"
 
-test "$(wc -c <"$rendered" | tr -d ' ')" = 21307
+test "$(wc -c <"$rendered" | tr -d ' ')" = 21303
 test "$(shasum -a 256 "$rendered" | awk '{print $1}')" = \
-  b658950b41c7c6cedbf0b0a86104f0ef3c5390bf6e1d5f1417f121b92d454dcd
+  6fe5f81e9cb0b033afc9c347612359cbf965377c970be90840806620ff0c8edf
 
 echo "ECS launch-template renderer matches the reviewed decoded production bootstrap"
